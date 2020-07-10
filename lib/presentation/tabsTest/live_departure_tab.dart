271941d1cf53d0d35291d225d73386c7e8759aba
Ansgar Lichter <lichteransgar@gmail.com>
2020-07-10 12:17:13 +0200

    designed departures view stop sign

--- lib/presentation/tabsTest/live_departure_tab.dart
+++ lib/presentation/tabsTest/live_departure_tab.dart
@@ -26,20 +26,55 @@ class LiveDepartureTabState extends State<LiveDepartureTab> {
 
   @override
   Widget build(BuildContext context) {
-
-    final textTheme = Theme.of(context).textTheme;
-
     return Center(
         child: Column(
-      children: <Widget>[
-        Container(
-          margin: const EdgeInsets.only(top: 30.0, bottom: 45.0, left: 15.0),
-          child: Text(widget.stop.name, style: textTheme.headline6),
-        ),
+        children: <Widget>[
+        _buildStopSign(),
         widget.departures != null
             ? DeparturesList(widget.departures)
             : Text(''),
       ],
     ));
+  }
+
+  Widget _buildStopSign() {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Container(
+        margin: const EdgeInsets.only(top: 30.0, bottom: 45.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                            width: 5.0, color: Colors.green[600]),
+                        bottom: BorderSide(
+                            width: 5.0, color: Colors.green[600]),
+                        left: BorderSide(
+                            width: 5.0, color: Colors.green[600])),
+                    color: Colors.yellow),
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: Text(widget.stop.name, style: textTheme.headline6),
+                )),
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(
+                          width: 5.0, color: Colors.green[600]),
+                      bottom: BorderSide(
+                          width: 5.0, color: Colors.green[600]),
+                      right: BorderSide(
+                          width: 5.0, color: Colors.green[600]),
+                    ),
+                    color: Colors.yellow),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 9.5, bottom: 9.5, right: 9.5),
+                  child: Icon(Icons.train),
+                )),
+          ],
+        ));
   }
 }
