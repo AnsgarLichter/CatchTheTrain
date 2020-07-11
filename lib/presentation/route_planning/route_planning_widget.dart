3b41caeaa3522fb192df9f97904ee400e3288472
Ansgar Lichter <lichteransgar@gmail.com>
2020-07-11 21:38:43 +0200

    #9: designed filter as bottom sheet

--- lib/presentation/route_planning/route_planning_widget.dart
+++ lib/presentation/route_planning/route_planning_widget.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class RoutePlanning extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            // center the children
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.train,
-                size: 160.0,
-                color: Theme.of(context).accentColor,
-              ),
-              Text(
-                "Route Planning",
-                style: TextStyle(color: Theme.of(context).primaryColor),
-              )
-            ],
-          ),
-        ),
-      ),
+      body: FloatingActionButton(
+          onPressed: _onFilterButtonPressed, child: Icon(FontAwesome.filter)),
     );
+  }
+
+  void _onFilterButtonPressed() {
+    return;
   }
 }
--- lib/presentation/tabsTest/live_departure_tab.dart
+++ lib/presentation/tabsTest/live_departure_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:myapp/models/departure.dart';
 import 'package:myapp/models/stop.dart';
 import 'package:myapp/presentation/loading_indicator.dart';
@@ -11,6 +12,7 @@ class LiveDepartureTab extends StatefulWidget {
   final Map<Stop, List<Departure>> departures;
   final String errorMessage;
   final Function(Stop, String) onLoadDepartures;
+  final _formKey = GlobalKey<FormState>();
 
   LiveDepartureTab(
       {@required this.stop,
@@ -35,114 +37,145 @@ class LiveDepartureTabState extends State<LiveDepartureTab> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onVerticalDragEnd: _onSwipeUp,
-        child: Center(
+      onVerticalDragEnd: _onSwipeUp,
+      child: Center(
         child: Column(
-      children: <Widget>[
-        _buildStopSign(),
-        _buildFilterLineForm(),
-        widget.errorMessage.isNotEmpty
-            ? Text(widget.errorMessage)
-            : widget.departures.length == 0
-                ? LoadingIndicator()
-                : DeparturesList(widget.departures[widget.stop]),
-      ],
-    )));
+          children: <Widget>[
+            _buildStopSign(),
+            //_buildFilterLineForm(),
+            widget.errorMessage.isNotEmpty
+                ? Text(widget.errorMessage)
+                : widget.departures.length == 0
+                    ? LoadingIndicator()
+                    : DeparturesList(widget.departures[widget.stop]),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                margin: EdgeInsets.only(top: 30.0, bottom: 15.0, right: 15.0),
+                child: FloatingActionButton(
+                    backgroundColor: line.isNotEmpty ? Theme.of(context).primaryColorDark : Theme.of(context).primaryColorLight,
+                    onPressed: () => _onFilterButtonPressed(context),
+                    child: Icon(FontAwesome.filter)),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Widget _buildStopSign() {
     final textTheme = Theme.of(context).textTheme;
     return Container(
-            margin: const EdgeInsets.only(top: 30.0, bottom: 15.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                DecoratedBox(
-                    decoration: BoxDecoration(
-                        border: Border(
-                            top: BorderSide(
-                                width: 5.0, color: Colors.green[600]),
-                            bottom: BorderSide(
-                                width: 5.0, color: Colors.green[600]),
-                            left: BorderSide(
-                                width: 5.0, color: Colors.green[600])),
-                        color: Colors.yellow),
-                    child: Container(
-                      margin: const EdgeInsets.only(
-                          left: 20.0, right: 15.0, top: 10.0, bottom: 10.0),
-                      child: Text(widget.stop.name, style: textTheme.headline6),
-                    )),
-                DecoratedBox(
-                    decoration: BoxDecoration(
-                        border: Border(
-                          top: BorderSide(width: 5.0, color: Colors.green[600]),
-                          bottom:
-                              BorderSide(width: 5.0, color: Colors.green[600]),
-                          right:
-                              BorderSide(width: 5.0, color: Colors.green[600]),
-                        ),
-                        color: Colors.yellow),
-                    child: Container(
-                      margin: const EdgeInsets.only(
-                          top: 9.5, bottom: 9.5, right: 20.0),
-                      child: Icon(Icons.train),
-                    )),
-              ],
-            ));
-  }
-
-  Widget _buildFilterLineForm() {
-    return Form(
-        autovalidate: true,
-        onChanged: () {
-          Form.of(primaryFocus.context).save();
-        },
+        margin: const EdgeInsets.only(top: 30.0, bottom: 30.0),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(
-                  left: 30.0, right: 15.0, top: 10.0, bottom: 30.0),
-              width: 100,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Filter', //TODO: localization
-                  labelText: 'Linie', //TODO: localization
-                ),
-                maxLength: 3,
-                validator: _onValidateForm,
-                onSaved: _onSaveForm,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(right: 15.0),
-              color: Theme.of(context).primaryColorLight,
-              child: IconButton(
-                icon: Icon(Icons.save),
-                tooltip: 'Speichern des Filters',
-                onPressed: _onSaveButtonPressed,
-              ),
-            )
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(width: 5.0, color: Colors.green[600]),
+                        bottom:
+                            BorderSide(width: 5.0, color: Colors.green[600]),
+                        left: BorderSide(width: 5.0, color: Colors.green[600])),
+                    color: Colors.yellow),
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      left: 20.0, right: 15.0, top: 10.0, bottom: 10.0),
+                  child: Text(widget.stop.name, style: textTheme.headline6),
+                )),
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    border: Border(
+                      top: BorderSide(width: 5.0, color: Colors.green[600]),
+                      bottom: BorderSide(width: 5.0, color: Colors.green[600]),
+                      right: BorderSide(width: 5.0, color: Colors.green[600]),
+                    ),
+                    color: Colors.yellow),
+                child: Container(
+                  margin:
+                      const EdgeInsets.only(top: 9.5, bottom: 9.5, right: 20.0),
+                  child: Icon(Icons.train),
+                )),
           ],
         ));
   }
 
   String _onValidateForm(String value) {
-    final regExp =
-        new RegExp(r'^[S]?[\d]{1,2}$', caseSensitive: false, multiLine: false);
-    if (!regExp.hasMatch(value))
-      return 'z. B: 1, S1, S32';
-    else
+    final regExp = new RegExp(r'^[S]?[\d]{1,2}$', caseSensitive: false, multiLine: false);
+    if(line.isNotEmpty && value.isEmpty) {
+      line = value;
+      widget.onLoadDepartures(widget.stop, line);
       return null;
+    }
+    else if (!regExp.hasMatch(value))
+      return 'z. B: 1, S1, S32';
+    else {
+      line = value;
+      return null;
+    }
   }
 
-  void _onSaveForm(String value) {
-    line = value;
-    widget.onLoadDepartures(widget.stop, line);
+  void _onBottomSheetFilterButtonPressed() {
+    // if (widget._formKey.currentState.validate())
+      widget.onLoadDepartures(widget.stop, line);
+      Navigator.pop(context);
   }
 
-  void _onSaveButtonPressed() {
-    return;
+  void _onFilterButtonPressed(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return Container(
+            margin: EdgeInsets.only(
+                top: 30.0, bottom: 30.0, left: 15.0, right: 15.0),
+            height: 170,
+            child: Form(
+              key: widget._formKey,
+              autovalidate: true,
+              onChanged: () {
+                Form.of(primaryFocus.context).save();
+              },
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      icon: Icon(FontAwesome.filter),
+                      hintText: 'Filtern der Linien', //TODO: localization
+                      labelText: 'Linie', //TODO: localization
+                    ),
+                    maxLength: 3,
+                    validator: _onValidateForm,
+                    initialValue: line,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 15.0, right: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        FloatingActionButton.extended(
+                          label: Text('Filter'), //TODO: Internationalization
+                          icon: Icon(FontAwesome.filter),
+                          backgroundColor: Theme.of(context).primaryColorLight,
+                          onPressed: _onBottomSheetFilterButtonPressed,
+                        ),
+                        FloatingActionButton.extended(
+                          label: Text('Speichern'), //TODO: Internationalization
+                          icon: Icon(Icons.save),
+                          backgroundColor: Theme.of(context).primaryColorLight,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 
   void _onSwipeUp(DragEndDetails details) {
